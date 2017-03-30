08e32a80bd2367b0b39fd00458d44784c34e45b9
Mohammed Zubair <Mohammed Zubair>
2017-03-30 12:59:33 +0400

    bugfix: no. of astricks

--- build/index.test.js
+++ build/index.test.js
@@ -3,9 +3,9 @@
 var _index = require('./index');
 
 test('replaces blacklisted words with asterisks', function () {
-  return expect((0, _index.sensitiveWords)('The name of the NX will be Nintendo Switch', ['Switch'])).toBe('The name of the NX will be Nintendo ***');
+  return expect((0, _index.sensitiveWords)('The name of the NX will be Nintendo Switch', ['Switch'])).toBe('The name of the NX will be Nintendo *****');
 });
 
 test('replaces multiple instances of blacklisted words with asterisks', function () {
-  return expect((0, _index.sensitiveWords)('The name of the NX will be Nintendo Switch. This Switch will be awesome', ['Switch'])).toBe('The name of the NX will be Nintendo ***. This *** will be awesome');
+  return expect((0, _index.sensitiveWords)('The name of the NX will be Nintendo Switch. This Switch will be awesome', ['Switch'])).toBe('The name of the NX will be Nintendo *****. This ***** will be awesome');
 });
--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,11 @@ import {sensitiveWords} from "./index"
 
 test('replaces blacklisted words with asterisks', () => (
   expect(sensitiveWords('The name of the NX will be Nintendo Switch', ['Switch']))
-  .toBe('The name of the NX will be Nintendo ***')
+  .toBe('The name of the NX will be Nintendo *****')
 ))
 
 
 test('replaces multiple instances of blacklisted words with asterisks', () => (
   expect(sensitiveWords('The name of the NX will be Nintendo Switch. This Switch will be awesome', ['Switch']))
-  .toBe('The name of the NX will be Nintendo *****. This *** will be awesome')
+  .toBe('The name of the NX will be Nintendo *****. This ***** will be awesome')
 ))
