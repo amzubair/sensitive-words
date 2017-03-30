b10d15b0e6b706c19e8a567c0984eed0337dc487
Mohammed Zubair <Mohammed Zubair>
2017-03-30 12:50:50 +0400

    updated 3 astricks to 5 astricks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export const sensitiveWords = (content, words) => (
   content.replace(
     new RegExp(words.join('|'), 'ig'), 
-    '***'
+    '*****'
   )
 )
 
--- src/index.test.js
+++ src/index.test.js
@@ -8,5 +8,5 @@ test('replaces blacklisted words with asterisks', () => (
 
 test('replaces multiple instances of blacklisted words with asterisks', () => (
   expect(sensitiveWords('The name of the NX will be Nintendo Switch. This Switch will be awesome', ['Switch']))
-  .toBe('The name of the NX will be Nintendo ***. This *** will be awesome')
+  .toBe('The name of the NX will be Nintendo *****. This *** will be awesome')
 ))
