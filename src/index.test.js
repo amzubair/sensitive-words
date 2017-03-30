db49550a32d7ba581db460d538bba5c2e0fc629b
Mohamed Jubair <amzubair@live.com>
2017-03-30 17:28:06 +0400

    added eslint config and jest config

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,9 @@
-import {sensitiveWords} from "./index"
+import {sensitiveWords} from './index'
 
 test('replaces blacklisted words with asterisks', () => (
   expect(sensitiveWords('The name of the NX will be Nintendo Switch', ['Switch']))
   .toBe('The name of the NX will be Nintendo *****')
 ))
-
 
 test('replaces multiple instances of blacklisted words with asterisks', () => (
   expect(sensitiveWords('The name of the NX will be Nintendo Switch. This Switch will be awesome', ['Switch']))
